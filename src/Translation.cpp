29540a0e1c9a5d3a1345e8c1854f0a9a8a1fbe94
Mathieu Dandurand <mathieusoftw@gmail.com>
2022-02-13 21:14:57 -0500

    Now using result and removed everything to switch to a more optimized way of dispatching the applets

--- src/Translation.cpp
+++ src/Translation.cpp
@@ -1,22 +1,10 @@
 #include <Translation.hpp>
 
-std::variant<std::size_t, Translation::TranslationError> Translation::TranslateInstruction(
+Result<bool, Translation::TranslationError> Translation::TranslateInstruction(
     const ZydisDecodedInstruction &instruction,
     const ZydisDecodedOperand operands[ZYDIS_MAX_OPERAND_COUNT_VISIBLE],
-    std::byte *out_buffer,
-    const std::size_t &out_buffer_size
+    MappedMemory& mapped_memory
 )
 {
-    try 
-    {
-        // Get the function to translate that instruction inside the hash map
-        auto translation_func = vm_translation_map.at(instruction.mnemonic);
-        // Call the function retrived from the map
-        return translation_func(instruction, operands, out_buffer, out_buffer_size);
-    }
-    catch(std::out_of_range& ex)
-    {
-        // Function not found, return error
-        return TranslationError::INSTRUCTION_NOT_FOUND;
-    }
+    return Ok(true);
 }
