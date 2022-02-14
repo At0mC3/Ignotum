3c42f2688c472e8a9f2b3fe03dab32f71cf65e46
Mathieu Dandurand <mathieusoftw@gmail.com>
2022-02-13 21:24:14 -0500

    Translation: added a function to support the SUB instruction

--- src/Translation.cpp
+++ src/Translation.cpp
@@ -1,10 +1,33 @@
 #include <Translation.hpp>
 
-Result<bool, Translation::TranslationError> Translation::TranslateInstruction(
+HOT_PATH FORCE_INLINE void SubInstLogic(
+        const ZydisDecodedOperand operands[ZYDIS_MAX_OPERAND_COUNT_VISIBLE],
+        MappedMemory& mapped_memory
+        )
+{
+    switch(operands[0].type)
+    {
+        case ZydisOperandType::ZYDIS_OPERAND_TYPE_REGISTER:
+            break;
+        default:
+            break;
+    }
+}
+
+HOT_PATH Result<bool, Translation::TranslationError> Translation::TranslateInstruction(
     const ZydisDecodedInstruction &instruction,
     const ZydisDecodedOperand operands[ZYDIS_MAX_OPERAND_COUNT_VISIBLE],
     MappedMemory& mapped_memory
 )
 {
+    switch(instruction.mnemonic)
+    {
+        case ZydisMnemonic::ZYDIS_MNEMONIC_SUB:
+            SubInstLogic(operands, mapped_memory);
+            break;
+        default:
+            return Err(TranslationError::INSTRUCTION_NOT_FOUND);
+    }
+    
     return Ok(true);
 }
