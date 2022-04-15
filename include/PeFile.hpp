ee9edb87080cc1c20a80d3bd4825db27a9bbdcff
Mathieu Dandurand <mathieusoftw@gmail.com>
2022-04-15 01:30:08 -0400

    Added the [[nodiscard]] flag to the functions to force a better standard

--- include/PeFile.hpp
+++ include/PeFile.hpp
@@ -19,29 +19,30 @@ class PeFile
 public:
     struct ImportedFunction { std::string name; std::uint32_t rva; };
     enum class LoadOption{ LAZY_LOAD, FULL_LOAD };
-    LoadOption m_load_option{ LoadOption::LAZY_LOAD };
 private:
     std::fstream m_file_handle;
-    Win32::Architecture m_arch;
+    LoadOption m_load_option{ LoadOption::LAZY_LOAD };
+    Win32::Architecture m_arch{ Win32::Architecture::NOT_SUPPORTED };
     std::uintmax_t m_nt_headers_offset{ 0 };
     Win32::IMAGE_NT_HEADERS32 nt_headers32{ 0 };
     Win32::IMAGE_NT_HEADERS64 nt_headers64{ 0 };
+    Win32::IMAGE_NT_HEADERS_HYBRID* nt_headers_hybrid{ nullptr };
     std::unordered_map<std::string, Win32::IMAGE_SECTION_HEADER> m_sections_map;
     std::unordered_map<std::string, std::vector<ImportedFunction>> m_imported_functions_map;
 private:
-    static Win32::Architecture FindArchitecture(PeFile& pe);
+    [[nodiscard]] static Win32::Architecture FindArchitecture(PeFile& pe);
 private:
-    std::uint32_t RvaToRaw(const std::uint32_t& rva);
-    bool MapImports(const std::string& dll_name, const std::uint32_t& first_thunk_rva);
-    std::optional<Win32::IMAGE_DATA_DIRECTORY> GetImportDirectory();
-    bool LoadImports();
-    void LoadSections();
-    bool LoadNtHeaders();
+    [[nodiscard]] std::uint32_t RvaToRaw(const std::uint32_t& rva) const;
+    [[nodiscard]] bool MapImports(const std::string& dll_name, const std::uint32_t& first_thunk_rva);
+    [[nodiscard]] std::optional<Win32::IMAGE_DATA_DIRECTORY> GetImportDirectory() const;
+    [[nodiscard]] bool LoadImports();
+    [[nodiscard]] bool LoadSections();
+    [[nodiscard]] bool LoadNtHeaders();
 public:
-    [[maybe_unused]] [[nodiscard]] std::uint32_t GetEntryPoint() const;
-    Result<bool, const char*> WriteToRegion(const std::uint32_t& rva, const MappedMemory& mapped_memory);
-    Result<MappedMemory, const char*> LoadRegion(const std::uint32_t& rva, const std::size_t& region_size);
-    Result<Win32::IMAGE_SECTION_HEADER, const char*> AddSection(const std::string_view& section_name);
+    [[nodiscard]] std::uint32_t GetEntryPoint() const;
+    [[nodiscard]] Result<bool, const char*> WriteToRegion(const std::uint32_t& rva, const MappedMemory& mapped_memory);
+    [[nodiscard]] Result<MappedMemory, const char*> LoadRegion(const std::uint32_t& rva, const std::size_t& region_size);
+    [[nodiscard]] Result<Win32::IMAGE_SECTION_HEADER, const char*> AddSection(const std::string_view& section_name);
     static Result<std::shared_ptr<PeFile>, const char*> Load(const std::filesystem::path& p, const LoadOption& load_option);
 };
 
