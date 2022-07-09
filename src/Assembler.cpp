0622a811b5902abd8e4bdc200cb1e9d097418a35
Mathieu Dandurand <mathieusoftw@gmail.com>
2022-07-08 21:00:31 -0400

    Assembler: Polished some memory writing

--- src/Assembler.cpp
+++ src/Assembler.cpp
@@ -5,12 +5,20 @@ bool X64::Generator::CallNear(MappedMemory& out_memory, std::int32_t relative_of
     const auto address_size = 0x05;
     relative_offset -= address_size;
 
-    out_memory.Write<std::uint8_t>(0xE8);
+    // Write to the buffer and check if it worked
+    if(!out_memory.Write<std::uint8_t>(0xE8)) {
+        return false;
+    }
+
     return out_memory.Write(relative_offset);
 }
 
 bool X64::Generator::PushX32(MappedMemory& out_memory, const std::uint32_t value)
 {
-    out_memory.Write<std::uint8_t>(0x68);
+    // Write to the buffer and check if it worked
+    if(!out_memory.Write<std::uint8_t>(0x68)) {
+        return false;
+    }
+
     return out_memory.Write<std::uint32_t>(value);
 }
